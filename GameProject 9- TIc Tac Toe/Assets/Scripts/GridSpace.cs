744e6a8475c07943aaf47d3e9450feb9cb54157d
Yaugan <yaugansd45@gmail.com>
2020-05-23 09:21:38 +0530

    Tic-Tac-Toe

---
+++ GameProject 9- TIc Tac Toe/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text textButton;
+    
+
+    private GameController gameController;
+
+
+    public void SetSpace()
+    {
+        if(gameController.playerMove == true)
+        {
+            textButton.text = gameController.GetPlayerSide();
+            button.interactable = false;
+            gameController.EndTurn();
+        }        
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+}
